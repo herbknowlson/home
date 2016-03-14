27f3cca1e40039d33c3bdeef0d7eaa11e0d846b7
Herb Knowlson <herbknowlson@gmail.com>
2016-03-14 00:22:03 -0500

    some changes

--- edit_hparse.bat
+++ edit_hparse.bat
@@ -1,4 +1,4 @@
 cd C:\Program Files (x86)\ActiveState Komodo Edit 9
-start komodo.exe E:\development\rule1\htmlParsev02.rb
+start komodo.exe E:\development\rule1\htmlParsev04.rb
 cd E:\development\rule1
 exit
